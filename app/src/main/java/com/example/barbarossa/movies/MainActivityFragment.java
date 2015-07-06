5c31bf48b23ebb0883217686296eb87ddc5ad1b0
Andrei Balaban <abalaban@bitdefender.com>
2015-07-06 22:06:19 +0300

    Test picasso.

--- app/src/main/java/com/example/barbarossa/movies/MainActivityFragment.java
+++ app/src/main/java/com/example/barbarossa/movies/MainActivityFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 
 /**
@@ -18,6 +21,17 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ImageView imv = (ImageView) rootView.findViewById(R.id.imageView);
+
+        Picasso mPicasso = Picasso.with(getActivity());
+//        mPicasso.setIndicatorsEnabled(true);
+
+        mPicasso.load("http://i.imgur.com/DvpvklR.png").into(imv);
+
+
+        return rootView;
     }
 }
